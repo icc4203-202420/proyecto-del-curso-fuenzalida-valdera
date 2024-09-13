77a34716c7cbfd1d4e08855d4e7409eb686d6650
DomingoFuenzalida <dfuenzalida2@miuandes.cl>
2024-09-13 15:47:41 -0300

    Mapa colocado pero está pegado

--- www-frontend/src/components/Map.jsx
+++ www-frontend/src/components/Map.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+"use client";
 
-const Map = () => {
+import React from "react";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+
+export default function Intro() {
+  const position = { lat: 37.7749, lng: -122.4194 };
+
   return (
-    <div>
-      <h1>Home/Map</h1>
-    </div>
+    <APIProvider apiKey="AIzaSyDPrIIA-Oy7AGgPFGXQ4nP0LOQ3TnIcI6U">
+      <div style={{ height: "93vh", width: "99vw"}}>
+        <Map zoom={9} center={position} style={{ height: "100%", width: "100%" }} />
+      </div>
+    </APIProvider>
   );
-};
-
-export default Map;
+}
