97d26d9182b706e9814d3424a351f22d7305ba63
JaValderaContador <javier1gdvc@gmail.com>
2024-09-29 20:33:24 -0300

    Better review colors

--- www-frontend/src/components/ReviewForm.jsx
+++ www-frontend/src/components/ReviewForm.jsx
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { TextField, Button, Typography } from '@mui/material';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { TextField, Button, Typography } from '@mui/material'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 const ReviewForm = () => {
-  const { id: beerId } = useParams(); // Obtiene el ID de la cerveza desde la URL
-  const [rating, setRating] = useState('');
-  const [text, setText] = useState('');
-  const [error, setError] = useState(null);
-  const [isSubmitting, setIsSubmitting] = useState(false); // Estado para controlar el envío
-  const [currentUserId, setCurrentUserId] = useState(null); // Estado para almacenar el ID del usuario
+  const { id: beerId } = useParams() // Obtiene el ID de la cerveza desde la URL
+  const [rating, setRating] = useState('')
+  const [text, setText] = useState('')
+  const [error, setError] = useState(null)
+  const [isSubmitting, setIsSubmitting] = useState(false) // Estado para controlar el envío
+  const [currentUserId, setCurrentUserId] = useState(null) // Estado para almacenar el ID del usuario
 
   useEffect(() => {
     const fetchUserId = () => {
-      const storedUserId = sessionStorage.getItem('userId');
+      const storedUserId = sessionStorage.getItem('userId')
       if (storedUserId) {
-        setCurrentUserId(Number(storedUserId)); // Asegúrate de que sea un número
+        setCurrentUserId(Number(storedUserId)) // Asegúrate de que sea un número
       } else {
-        setError('User ID not found in session storage');
+        setError('User ID not found in session storage')
       }
-    };
+    }
 
-    fetchUserId();
-  }, []); // Este efecto se ejecuta una sola vez al montar el componente
+    fetchUserId()
+  }, []) // Este efecto se ejecuta una sola vez al montar el componente
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
-    const token = sessionStorage.getItem('jwtToken');
-  
+    event.preventDefault()
+    const token = sessionStorage.getItem('jwtToken')
+
     // Validar el rating
     if (!rating || rating < 1 || rating > 5) {
-      setError('Rating must be a number between 1 and 5.');
-      return;
+      setError('Rating must be a number between 1 and 5.')
+      return
     }
-  
+
     // Cambiar la estructura de datos para el POST
     const data = {
       review: {
@@ -41,35 +41,35 @@ const ReviewForm = () => {
         text,
       },
       user_id: currentUserId, // Incluyendo el user_id directamente
-    };
-  
+    }
+
     try {
-      setIsSubmitting(true);
+      setIsSubmitting(true)
       const response = await axios.post(`http://localhost:3001/api/v1/beers/${beerId}/reviews`, data, {
         headers: { Authorization: `Bearer ${token}` }, // Mantén el token para otras autenticaciones
-      });
-  
+      })
+
       if (response.status === 201) {
-        alert('Review added successfully!');
-        setRating('');
-        setText('');
-        setError(null);
+        alert('Review added successfully!')
+        setRating('')
+        setText('')
+        setError(null)
       }
     } catch (error) {
-      console.error('Error adding review:', error);
+      console.error('Error adding review:', error)
       if (error.response && error.response.data && error.response.data.errors) {
-        setError(error.response.data.errors.join(', '));
+        setError(error.response.data.errors.join(', '))
       } else {
-        setError('Failed to add review. Please try again.');
+        setError('Failed to add review. Please try again.')
       }
     } finally {
-      setIsSubmitting(false);
+      setIsSubmitting(false)
     }
-  };  
+  }
 
   return (
     <form onSubmit={handleSubmit}>
-      <Typography variant="h6">Add a Review</Typography>
+      <Typography variant="h6" style={{ color: 'white' }}>Add a Review</Typography>
       <TextField
         label="Rating (1-5)"
         variant="outlined"
@@ -78,6 +78,8 @@ const ReviewForm = () => {
         value={rating}
         onChange={(e) => setRating(e.target.value)}
         required
+        InputProps={{ style: { color: 'white' } }} // Cambiar color del texto a blanco
+        InputLabelProps={{ style: { color: 'white' } }} // Cambiar color de la etiqueta a blanco
       />
       <TextField
         label="Review Text"
@@ -88,13 +90,15 @@ const ReviewForm = () => {
         value={text}
         onChange={(e) => setText(e.target.value)}
         required
+        InputProps={{ style: { color: 'white' } }} // Cambiar color del texto a blanco
+        InputLabelProps={{ style: { color: 'white' } }} // Cambiar color de la etiqueta a blanco
       />
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <Button type="submit" variant="contained" color="primary" disabled={isSubmitting}>
         {isSubmitting ? 'Submitting...' : 'Submit Review'}
       </Button>
     </form>
-  );
-};
+  )
+}
 
-export default ReviewForm;
+export default ReviewForm
