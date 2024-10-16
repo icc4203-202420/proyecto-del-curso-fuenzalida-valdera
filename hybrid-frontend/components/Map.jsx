217c8ed429268b92691e3e4324f98772bfa8f79d
JaValderaContador <javier1gdvc@gmail.com>
2024-10-16 16:37:53 -0300

    Map in React native

--- hybrid-frontend/components/Map.jsx
+++ hybrid-frontend/components/Map.jsx
@@ -1,11 +1,9 @@
-import { Loader } from '@googlemaps/js-api-loader'
-import { MarkerClusterer } from '@googlemaps/markerclusterer'
-import { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { View, Text, StyleSheet, ActivityIndicator, Alert } from 'react-native'
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import axios from 'axios'
-import { TextField, Button } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
+import { Input, Button } from 'react-native-elements'
 
-// Función para calcular la distancia entre dos puntos geográficos
 const calculateDistance = (lat1, lng1, lat2, lng2) => {
   const R = 6371 // Radio de la Tierra en kilómetros
   const dLat = (lat2 - lat1) * (Math.PI / 180)
@@ -20,7 +18,7 @@ const calculateDistance = (lat1, lng1, lat2, lng2) => {
   return R * c // Distancia en kilómetros
 }
 
-const Map = () => {
+const Map = ({ navigation }) => {
   const [bars, setBars] = useState([])
   const [filteredBars, setFilteredBars] = useState([])
   const [loading, setLoading] = useState(true)
@@ -29,12 +27,7 @@ const Map = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [searchResultPosition, setSearchResultPosition] = useState(null)
   const mapRef = useRef(null)
-  const markerClusterRef = useRef(null)
-  const markersRef = useRef([])
-  const infoWindowRef = useRef(null)
-  const navigate = useNavigate()
 
-  // Función para obtener los bares desde la API
   const fetchBars = async () => {
     try {
       const response = await axios.get('http://localhost:3001/api/v1/bars')
@@ -46,47 +39,6 @@ const Map = () => {
     }
   }
 
-  // Función para añadir marcadores al mapa
-  const addMarker = (location, name, id, map, markerClusterer) => {
-    const marker = new google.maps.Marker({
-      position: location,
-      map: map,
-    })
-
-    marker.addListener('click', () => {
-      if (infoWindowRef.current) {
-        infoWindowRef.current.close()
-      }
-      const infoWindow = new google.maps.InfoWindow({
-        content: `
-          <div style="font-size: 14px background-color: #fff border: 2px solid #000 border-radius: 5px padding: 10px">
-            <h4 style="margin: 0 color: #1D1B20">${name}</h4>
-            <p style="margin: 5px 0 color: #1D1B20">${location.lat.toFixed(2)}, ${location.lng.toFixed(2)}</p>
-            <button id="info-window-button-${id}" 
-              style="background-color: #3f51b5 color: #fff border: none padding: 8px 16px border-radius: 4px cursor: pointer">
-              See more
-            </button>
-          </div>`,
-        position: location,
-      })
-      infoWindow.open(map, marker)
-      infoWindowRef.current = infoWindow
-
-      google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
-        const button = document.getElementById(`info-window-button-${id}`)
-        if (button) {
-          button.addEventListener('click', () => {
-            navigate(`/bars/${id}`)
-          })
-        }
-      })
-    })
-
-    markerClusterer.addMarker(marker)
-    markersRef.current.push(marker)
-  }
-
-  // Cargar la información de los bares al inicializar el componente
   useEffect(() => {
     const initializeMap = async () => {
       setLoading(true)
@@ -99,88 +51,15 @@ const Map = () => {
     initializeMap()
   }, [])
 
-  // Inicializar el mapa y los marcadores
-  useEffect(() => {
-    if (!mapRef.current) return
-
-    const loader = new Loader({
-      apiKey: import.meta.env.VITE_GOOGLE_MAPS_KEY,
-      version: 'weekly',
-    })
-
-    loader
-      .importLibrary('maps')
-      .then((lib) => {
-        const { Map } = lib
-        const map = new Map(mapRef.current, {
-          center: userLocation || { lat: 0, lng: 0 },
-          zoom: 12,
-        })
-
-        const markerClusterer = new MarkerClusterer(map, [], {})
-        markerClusterRef.current = markerClusterer
-
-        return { map, markerClusterer }
-      })
-      .then(({ map, markerClusterer }) => {
-        filteredBars.forEach(({ name, latitude, longitude, id }) => {
-          const position = { lat: latitude, lng: longitude }
-          addMarker(position, name, id, map, markerClusterer)
-        })
-
-        if (searchResultPosition) {
-          map.setCenter(searchResultPosition)
-        }
-      })
-      .catch((error) => {
-        console.error('Error loading Google Maps library:', error)
-      })
-  }, [filteredBars, userLocation, searchResultPosition])
-
-  // Manejo de búsqueda de bares
-  const handleSearch = () => {
-    const filtered = bars.filter(bar => {
-      const nameMatch = bar.name.toLowerCase().includes(searchTerm.toLowerCase())
-      const addressMatch = bar.address.line1.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                           bar.address.line2?.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                           bar.address.city.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                           bar.address.country.name.toLowerCase().includes(searchTerm.toLowerCase())
-
-      return nameMatch || addressMatch
-    })
-
-    setFilteredBars(filtered)
-
-    if (userLocation && filtered.length > 0) {
-      // Encuentra el bar más cercano a la ubicación del usuario
-      const closestBar = filtered.reduce((closest, bar) => {
-        const distance = calculateDistance(userLocation.lat, userLocation.lng, bar.latitude, bar.longitude)
-        if (distance < closest.distance) {
-          return { ...bar, distance }
-        }
-        return closest
-      }, { distance: Infinity })
-
-      const closestPosition = { lat: closestBar.latitude, lng: closestBar.longitude }
-      setSearchResultPosition(closestPosition)
-    }
-  }
-
-  // Obtener la ubicación del usuario
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          const userPos = { lat: position.coords.latitude, lng: position.coords.longitude }
-          setUserLocation(userPos)
-
-          if (mapRef.current) {
-            const map = new google.maps.Map(mapRef.current, {
-              center: userPos,
-              zoom: 12,
-            })
-            markerClusterRef.current.setMap(map)
+          const userPos = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
           }
+          setUserLocation(userPos)
         },
         (error) => {
           console.error('Error getting user location:', error)
@@ -191,27 +70,98 @@ const Map = () => {
     }
   }, [])
 
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>{error}</div>
+  const handleSearch = () => {
+    const filtered = bars.filter((bar) => {
+      const nameMatch = bar.name.toLowerCase().includes(searchTerm.toLowerCase())
+      const addressMatch =
+        bar.address.line1.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        bar.address.line2?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        bar.address.city.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        bar.address.country.name.toLowerCase().includes(searchTerm.toLowerCase())
+
+      return nameMatch || addressMatch
+    })
+
+    setFilteredBars(filtered)
+
+    if (userLocation && filtered.length > 0) {
+      const closestBar = filtered.reduce((closest, bar) => {
+        const distance = calculateDistance(userLocation.latitude, userLocation.longitude, bar.latitude, bar.longitude)
+        if (distance < closest.distance) {
+          return { ...bar, distance }
+        }
+        return closest
+      }, { distance: Infinity })
+
+      const closestPosition = { latitude: closestBar.latitude, longitude: closestBar.longitude }
+      setSearchResultPosition(closestPosition)
+    }
+  }
+
+  if (loading) return <ActivityIndicator size="large" color="#0000ff" />
+  if (error) return <Text>{error}</Text>
 
   return (
-    <div>
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '16px', marginTop: '16px' }}>
-        <TextField
-          label="Search Bars"
-          variant="outlined"
-          fullWidth
-          onChange={(e) => setSearchTerm(e.target.value)}
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <Input
+          placeholder="Search Bars"
+          onChangeText={setSearchTerm}
           value={searchTerm}
-          style={{ backgroundColor: 'white', marginRight: '8px' }} // Color de fondo y margen para espaciado
+          containerStyle={styles.input}
         />
-        <Button variant="contained" onClick={handleSearch}>
-          Search
-        </Button>
-      </div>
-      <div ref={mapRef} style={{ width: '98vw', height: '78vh' }} />
-    </div>
+        <Button title="Search" onPress={handleSearch} />
+      </View>
+      {userLocation && (
+        <MapView
+          ref={mapRef}
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: userLocation.latitude,
+            longitude: userLocation.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          {filteredBars.map(({ name, latitude, longitude, id }) => (
+            <Marker
+              key={id}
+              coordinate={{ latitude, longitude }}
+              title={name}
+              description={`${latitude.toFixed(2)}, ${longitude.toFixed(2)}`}
+              onPress={() => navigation.navigate('BarDetails', { id })}
+            />
+          ))}
+          {searchResultPosition && (
+            <Marker
+              coordinate={searchResultPosition}
+              pinColor="blue" // Color para la marca de resultado de búsqueda
+            />
+          )}
+        </MapView>
+      )}
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  input: {
+    flex: 1,
+    marginRight: 8,
+  },
+  map: {
+    flex: 1,
+  },
+})
 
 export default Map
