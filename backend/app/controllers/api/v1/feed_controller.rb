5f97b978537136d2b3336192cbbfbdc2fb3c63fb
JaValderaContador <javier1gdvc@gmail.com>
2024-11-13 02:24:59 -0300

    Feed only Friends and self

--- backend/app/controllers/api/v1/feed_controller.rb
+++ backend/app/controllers/api/v1/feed_controller.rb
@@ -1,30 +1,42 @@
 class API::V1::FeedController < ApplicationController
   def index
-    event_pictures = EventPicture.includes(:event, :user).order(created_at: :desc)
+    user_id = params[:user_id]
+    if user_id.nil?
+      render json: { error: 'User ID is missing' }, status: :unauthorized and return
+    end
 
-    reviews = Review.includes(:user, :beer).order(created_at: :desc)
+    current_user = User.find_by(id: user_id)
+    unless current_user
+      render json: { error: 'Invalid user ID' }, status: :unauthorized and return
+    end
+
+    friend_ids = current_user.friends.pluck(:id)
+
+
+    event_pictures = EventPicture.includes(:event, :user).where(user_id: [current_user.id, *friend_ids]).order(created_at: :desc)
+    reviews = Review.includes(:user, :beer).where(user_id: [current_user.id, *friend_ids]).order(created_at: :desc)
 
     feed = event_pictures.map do |event_picture|
       {
         type: 'event_picture',
-        image_url: url_for(event_picture.image),  
-        description: event_picture.description,  
-        created_at: event_picture.created_at.iso8601,    
-        event_name: event_picture.event.name,   
+        image_url: url_for(event_picture.image),
+        description: event_picture.description,
+        created_at: event_picture.created_at.iso8601,
+        event_name: event_picture.event.name,
         user_name: event_picture.user.handle,
         bar_id: event_picture.event.bar_id,
-        event_id: event_picture.event_id      
+        event_id: event_picture.event_id
       }
     end
 
     reviews.each do |review|
       feed.push({
-        type: 'beer_review',  
-        beer_name: review.beer.name,  
-        rating: review.rating, 
-        review_text: review.text,  
-        created_at: review.created_at.iso8601,  
-        user_name: review.user.handle ,
+        type: 'beer_review',
+        beer_name: review.beer.name,
+        rating: review.rating,
+        review_text: review.text,
+        created_at: review.created_at.iso8601,
+        user_name: review.user.handle,
         beer_id: review.beer.id
       })
     end
--- hybrid-frontend/components/Feed.jsx
+++ hybrid-frontend/components/Feed.jsx
@@ -14,24 +14,24 @@ const Feed = () => {
     const fetchFeed = async () => {
       try {
         const token = await SecureStore.getItemAsync('jwtToken');
-        if (token) {
-          const response = await fetch(`${backend_url}/api/v1/feed`, {
+        const userId = await SecureStore.getItemAsync('userId');
+        if (token && userId) {
+          const response = await fetch(`${backend_url}/api/v1/feed?user_id=${userId}`, {
+            method: 'GET',
             headers: { Authorization: `Bearer ${token}` },
           });
-
+    
           if (response.ok) {
             const data = await response.json();
             setFeed(data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)));
           } else {
             console.error('Failed to fetch feed');
-          }
+          }    
         }
-      } catch (error) {
-        console.error('Error fetching feed:', error);
       } finally {
         setLoading(false);
       }
-    };
+    };    
 
     fetchFeed();
   }, []);
